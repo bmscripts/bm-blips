7289f0aff925051687dfccd56755a3daf75f8f87
BM Scripts <contactbmscripts@gmail.com>
2026-01-17 11:14:01 +0000

    Uploaded files

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'BM Blips'
+author 'BM Scripts'
+description 'Display any blips in any location of the map'
+version '1.0.0'
+
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+--dependency 'blip_info'
